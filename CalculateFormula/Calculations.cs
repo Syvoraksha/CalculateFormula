20616821973d9e823c0723df97d8776f7f393fbd
Vladymyr <syvorakshav@gmail.com>
2020-04-22 22:34:31 +0300

    Add project files.

---
+++ CalculateFormula/Calculations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateFormula
+{
+    public class Calculations
+    {
+        public string ReturnResult(double a, double b, double c, double d, double e, double f)
+        {
+            double result = a + b * c / Math.Pow((d - e), f);
+            return result.ToString();
+        }
+    }
+}
